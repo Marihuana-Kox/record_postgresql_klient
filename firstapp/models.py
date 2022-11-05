23556d6dadc43c7f6e7b098e350ba929579a22ec
Marihuana-Kox <yakuhs@yandex.ru>
2022-11-05 21:56:16 +0300

    Добавлено предстовление и шаблоны

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class MainStartPage(models.Model):
+    '''Главные страницы сайта'''
+    slug = models.SlugField(max_length=30, unique=True, default="slug")
+    title = models.CharField("Короткое название", max_length=150)
+    description = models.CharField("Краткое описание", max_length=250)
+    text = models.TextField("Текст статьи")
+    image = models.ImageField(upload_to='pages/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Страница'
+        verbose_name_plural = 'Страницы'
+
+
+class CategoryArticles(models.Model):
+    '''Категории статей'''
+    title = models.CharField("Короткое название", max_length=150)
+    description = models.CharField("Краткое описание", max_length=250)
+    image = models.ImageField(upload_to='categorys/',
+                              blank=True, null=True)  # поле для картинки
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Articles(models.Model):
+    '''Статьи'''
+    title = models.CharField("Короткое название", max_length=150)
+    description = models.CharField("Краткое описание", max_length=250)
+    text = models.TextField("Текст статьи")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    image = models.ImageField(upload_to='articles/',
+                              blank=True, null=True)  # поле для картинки
+    category = models.ForeignKey(CategoryArticles, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+from .models import *
 
-# Create your views here.
+
+class MainStartPage(View):
+    '''Вывод главной страницы'''
+    def get(self, request):
+        template_name = 'firstapp/content.html'
+        queryset = MainStartPage.objects.get(pk=1)
+        return render(request, template_name)
